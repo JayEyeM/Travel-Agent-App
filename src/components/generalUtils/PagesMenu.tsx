aa1867d228fd2c464caa5513f2fd4ad27e3fa2fc
JayEye <136415777+JayEyeM@users.noreply.github.com>
2024-08-26 02:26:58 -0230

    page routing and layout/design

--- src/components/generalUtils/PagesMenu.tsx
+++ src/components/generalUtils/PagesMenu.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Box, Text, Button } from '@chakra-ui/react';
+import { Box, Text, Button, SimpleGrid } from '@chakra-ui/react';
 import { useBrandColors } from '../generalUtils/theme';
 
 interface PagesMenuProps {
@@ -14,57 +14,74 @@ const PagesMenu: React.FC<PagesMenuProps> = ({ currentPage }) => {
         <Box
             bg={brand900}
             color={brand600}
-            p={4}
+            p={2}
             borderRadius="lg"
             boxShadow="rgba(0, 0, 0, 0.35) 0px 5px 15px;"
-            mt={4}
             w={'100%'}
-            maxW={'200px'}
+            maxW={{ base: '100%', md: '200px' }}
             h={'auto'}
-            display={'flex'}
-            flexDirection={'column'}
-            justifyContent={'center'}
-            alignItems={'center'}
+            maxH={{ base: 'auto', md: '500px' }}
+            textAlign={{ base: 'center', md: 'left' }}
+            
         >
-            <Text fontSize="lg" fontWeight="bold" mb={4}>
+            <Text fontSize="lg" fontWeight="bold" mb={4} display={{ base: 'none', md: 'block' }}>
                 Menu
             </Text>
-            <Button
-                as={Link}
-                to="/"
-                variant="link"
-                color={currentPage === 'home' ? brand700 : brand800}
-                _hover={{ color: brand700 }}
+            <SimpleGrid
+                columns={{ base: 2, md: 1 }}
+                spacing={{ base: 2, md: 4 }}
+                w={'100%'}
+                h={'auto'}
+                justifyItems={{ base: 'center', md: 'left' }}
+                p={2}
+
             >
-                Home
-            </Button>
-            <Button
-                as={Link}
-                to="/calculator"
-                variant="link"
-                color={currentPage === 'calculator' ? brand700 : brand800}
-                _hover={{ color: brand700 }}
-            >
-                Commission Calculator
-            </Button>
-            <Button
-                as={Link}
-                to="/dashboard"
-                variant="link"
-                color={currentPage === 'dashboard' ? brand700 : brand800}
-                _hover={{ color: brand700 }}
-            >
-                Dashboard
-            </Button>
-            <Button
-                as={Link}
-                to="/about"
-                variant="link"
-                color={currentPage === 'about' ? brand700 : brand800}
-                _hover={{ color: brand700 }}
-            >
-                About
-            </Button>
+                <Button
+                    as={Link}
+                    to="/"
+                    variant="link"
+                    color={currentPage === 'home' ? brand700 : brand800}
+                    _hover={{ color: brand700 }}
+                >
+                    Home
+                </Button>
+                <Button
+                    as={Link}
+                    to="/calculator"
+                    variant="link"
+                    color={currentPage === 'calculator' ? brand700 : brand800}
+                    _hover={{ color: brand700 }}
+                >
+                    Commission Calculator
+                </Button>
+                <Button
+                    as={Link}
+                    to="/dashboard"
+                    variant="link"
+                    color={currentPage === 'dashboard' ? brand700 : brand800}
+                    _hover={{ color: brand700 }}
+                >
+                    Dashboard
+                </Button>
+                <Button
+                    as={Link}
+                    to="/projectManagement"
+                    variant="link"
+                    color={currentPage === 'projectManagement' ? brand700 : brand800}
+                    _hover={{ color: brand700 }}
+                >
+                    Project Management
+                </Button>
+                <Button
+                    as={Link}
+                    to="/about"
+                    variant="link"
+                    color={currentPage === 'about' ? brand700 : brand800}
+                    _hover={{ color: brand700 }}
+                >
+                    About
+                </Button>
+            </SimpleGrid>
         </Box>
     );
 };
