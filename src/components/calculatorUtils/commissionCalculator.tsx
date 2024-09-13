87b7588fba5a50c5646b55ff3d579fcd7eaf325d
JayEye <136415777+JayEyeM@users.noreply.github.com>
2024-09-13 19:01:36 -0230

    Working on Data Display.
    Data display for commissions related data, to be an overview on Dashboard and bottom of Commissions pages.

--- src/components/calculatorUtils/commissionCalculator.tsx
+++ src/components/calculatorUtils/commissionCalculator.tsx
@@ -4,49 +4,51 @@ import CommisionForm from './commissionForm'
 import { useBrandColors } from '../generalUtils/theme'
 import DisplayCommissions from './displayCommissions';
 import useCommissionsData from './useCommissionsDataHook';
+import DataDisplay from '../generalUtils/DataDisplay';
+import ClosableBox from '../generalUtils/ClosableBox';
 
 const CommisionCalculator = () => {
     const { primary, background, accent, secondary, text } = useBrandColors()
-    const { commissions } = useCommissionsData();
-    const [totalCommissionRateAmount, setTotalCommissionRateAmount] = useState(0);
     
-    useEffect(() => {
-      const total = commissions.reduce((acc, commission) => acc + commission.commissionRateAmount, 0);
-      setTotalCommissionRateAmount(total);
-    }, [commissions]);
     
     
     
     
 
   return (
-    <Box p={0} ml={'auto'} mr={'auto'} w={'90%'} borderRadius="lg" bg={background}>
-      <Box
-        p={4}
-        mt={0}
-        outline="2px solid"
-        outlineColor="white"
-        borderRadius="lg"
-        bg={background}
-      >
-        <Heading>Commision Calculator</Heading>
-        <Text fontSize="lg" color={text}>
-          Fill in the form to calculate your commission
-        </Text>
-      </Box>
-      <CommisionForm />
-      <DisplayCommissions />
+    <Box textAlign={'center'} py={10} px={0} ml={'auto'} mr={'auto'} w={'90%'} borderRadius="lg" bg={background}>
+      
+      <Heading as="h1" size="xl" mb={4}>
+       Commission Management
+      </Heading>
+      <Text fontSize="lg">
+        Add new clients and keep track of them below. Click on a client in the Client List to view more 
+        details and use handy tools, such as the Trip Planner and Commission Calculator.
+      </Text>
+      
+
+      <ClosableBox buttonText='Calculate Commission' 
+      title="Commission Calculator" 
+      children={<CommisionForm />}
+      onClose={() => console.log('Close button clicked')}
+      onOpen={() => console.log('Open button clicked')}
+       /> 
+      
+      
+      <ClosableBox buttonText='View Commissions' 
+      title="Commissions" 
+      children={<DisplayCommissions />}
+      onClose={() => console.log('Close button clicked')}
+      onOpen={() => console.log('Open button clicked')}
+       /> 
+      
       <Box
         p={4}
         mt={4}
-        outline="2px solid"
-        outlineColor="white"
-        borderRadius="lg"
-        bg={background}
+        
       >
-        <Text fontSize="lg" color={accent}>
-        Total Commission Rate Amount: <Text as="b" fontSize={"xl"} color={secondary}>${totalCommissionRateAmount.toFixed(2)}</Text>
-        </Text>
+        <DataDisplay />
+        
         
        
       </Box>
